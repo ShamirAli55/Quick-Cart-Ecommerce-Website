5d370d6de15b4c57897f4d488810a38b148ed555
ShamirAli55 <shamirali9779@gmail.com>
2025-09-08 12:42:35 +0500

    routes configured

---
+++ app/api/inngest/route.js
@@ -0,0 +1,17 @@
+import { serve } from "inngest/next";
+import {
+  inngest,
+  syncUserCreation,
+  syncUserDeletion,
+  syncUserUpdate,
+} from "@/config/inngest";
+
+export const { GET, POST, PUT } = serve({
+  client: inngest,
+  functions: 
+[
+    syncUserCreation, 
+    syncUserUpdate, 
+    syncUserDeletion
+],
+});
